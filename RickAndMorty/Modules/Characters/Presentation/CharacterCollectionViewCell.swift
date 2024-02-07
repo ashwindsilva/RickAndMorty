a6884e38d35c65dbb61a537cbf1956fa1d4bc0eb
Ashwin <ashwinmariodsilva@gmail.com>
2024-02-07 12:48:25 +0530

    feat: CharacterViewController - add collection view with CharacterCollectionViewCell

---
+++ RickAndMorty/Modules/Characters/Presentation/CharacterCollectionViewCell.swift
@@ -0,0 +1,99 @@
+//
+//  CharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Ashwin D'Silva on 07/02/24.
+//
+
+import UIKit
+
+final class CharacterCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static let reuseIdentifier = "CharacterCollectionViewCell"
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        return imageView
+    }()
+    
+    private var overlayView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        
+        return view
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        label.text = "Sample text"
+        
+        return label
+    }()
+    
+    // MARK: - Methods
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        constructHierarchy()
+        activateConstraints()
+    }
+    
+    private func constructHierarchy() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(overlayView)
+        contentView.addSubview(label)
+    }
+    
+    private func activateConstraints() {
+        activateImageViewConstraints()
+        activateOverlayViewConstraints()
+        activateLabelConstraints()
+    }
+    
+    private func activateImageViewConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+        ])
+    }
+    
+    private func activateOverlayViewConstraints() {
+        NSLayoutConstraint.activate([
+            overlayView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            overlayView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            overlayView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor)
+        ])
+    }
+    
+    private func activateLabelConstraints() {
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: overlayView.topAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: overlayView.trailingAnchor, constant: -8),
+            label.bottomAnchor.constraint(equalTo: overlayView.bottomAnchor, constant: -8),
+            label.leadingAnchor.constraint(equalTo: overlayView.leadingAnchor, constant: 8)
+        ])
+    }
+}
--- RickAndMorty/Modules/Characters/Presentation/CharactersView.swift
+++ RickAndMorty/Modules/Characters/Presentation/CharactersView.swift
@@ -13,16 +13,98 @@ class CharactersView: UIView {
     
     let viewModel: CharactersViewModel
     
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
+    
     // MARK: - Methods
     
     init(viewModel: CharactersViewModel) {
         self.viewModel = viewModel
         super.init(frame: .zero)
         
-        backgroundColor = .systemBackground
+        setupUI()
+        configureCollectionView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupUI() {
+        backgroundColor = .systemBackground
+        addSubview(collectionView)
+        activateCollectionViewConstraints()
+    }
+    
+    private func activateCollectionViewConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor)
+        ])
+    }
+    
+    private func configureCollectionView() {
+        collectionView.register(
+            CharacterCollectionViewCell.self,
+            forCellWithReuseIdentifier: CharacterCollectionViewCell.reuseIdentifier
+        )
+    }
+    
+    private func makeCollectionViewLayout() -> UICollectionViewLayout {
+        let item = NSCollectionLayoutItem(
+            layoutSize: .init(
+                widthDimension: .fractionalWidth(0.5),
+                heightDimension: .fractionalWidth(0.5)
+            )
+        )
+        item.contentInsets.trailing = 16
+        item.contentInsets.bottom = 16
+        
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: .init(
+                widthDimension: .fractionalWidth(1),
+                heightDimension: .estimated(300)
+            ),
+            subitems: [item]
+        )
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: 16,
+            leading: 16,
+            bottom: 0,
+            trailing: 0
+        )
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+}
+
+extension CharactersView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: CharacterCollectionViewCell.reuseIdentifier,
+            for: indexPath) as? CharacterCollectionViewCell
+        else {
+            fatalError("Failed to dequeue CharacterCollectionViewCell")
+        }
+        
+        return cell
+    }
+}
+
+#Preview {
+    CharactersView(viewModel: .init())
 }
